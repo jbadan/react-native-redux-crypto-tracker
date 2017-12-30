2cb1197bbd630d8dd17dbbc6702858796e3dd421
jbadan <jenna.badanowski@gmail.com>
2017-12-29 17:09:04 -0800

    finish implementing reducer- error handling

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,8 @@
+import {
+  FETCHING_COIN_DATA,
+  FETCHING_COIN_DATA_SUCCESS,
+  FETCHING_COIN_DATA_FAIL
+} from './../Utils/ActionTypes';
 
 const initialState = {
   isFetching: false,
@@ -7,5 +12,29 @@ const initialState = {
 }
 
 export default function(state = initialState, action){
-  return state;
+  switch(action.type){
+    case FETCHING_COIN_DATA:
+      return Object.assign(state, {
+        isFetching: true,
+        data: null,
+        hasError: false,
+        errorMessage: null
+      });
+    case FETCHING_COIN_DATA_SUCCESS:
+      return Object.assign(state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: false,
+        errorMessage: null
+      });
+      case FETCHING_COIN_DATA_FAIL:
+        return Object.assign(state, {
+          isFetching: false,
+          data: null,
+          hasError: true,
+          errorMessage: action.payload
+        });
+    default:
+      return state;
+  }
 }
