9c7008b30922f0d16dff9fdb4e1bba8de75178d4
jbadan <jenna.badanowski@gmail.com>
2017-12-29 17:17:57 -0800

    coincard setup

---
+++ CoinCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image
+} from 'react-native';
+
+const CoinCard = (props) => {
+  return (
+    <View>
+      <Image />
+      <Text> {props.symbol} </Text>
+      <Text> {props.price_usd} </Text>
+      <Text> {props.percent_change_24h} </Text>
+    </View>
+  )
+}
+
+export default CoinCard;
