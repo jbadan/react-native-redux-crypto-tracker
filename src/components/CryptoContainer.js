29dfee5101ffeb631c4583ca12a543e7a6da416d
jbadan <jenna.badanowski@gmail.com>
2017-12-29 17:29:19 -0800

    coin card display

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import FetchCoinData from './../Actions/FetchCoinData';
+import CoinCard from './CoinCard';
 
 class CryptoContainer extends Component {
   componentDidMount(){
     this.props.FetchCoinData();
   }
+  renderCoinCard(){
+    const { crypto } = this.props;
+    console.log(crypto);
+    return crypto.data.map((coin, index) =>
+      <CoinCard
+        key={index}
+        coin_name={coin.name}
+        symbol = {coin.symbol}
+        price_usd = {coin.price_usd}
+        percent_change_24h = {coin.percent_change_24h}
+        percent_change_7d = {coin.percent_change_7d}
+      />
+    )
+  }
+
   render(){
+    const { crypto } = this.props;
+
     return(
       <View>
-        <Text> Container </Text>
+        {this.renderCoinCard()}
       </View>
     )
   }
